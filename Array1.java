4748db57fd6615ff24f3207702aae950937e8d8b
chipademayur <101926125+chipademayur@users.noreply.github.com>
2023-03-20 12:48:16 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,16 @@
+// Ages of student using array
+public class Array1 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // int ArrayList[]=new int[5];
+        int ArrayList[]={12,13,14,15,16};
+        for(int i=0;i<5;i++){
+            System.out.println("Age of student is:"+(i+1)+"  is:"+ArrayList[i]);
+    }
+    // for(int i=0;i<5;i++){
+    //     System.out.println("Age is:"+ArrayList[i]);
+    // }
+}
+}
